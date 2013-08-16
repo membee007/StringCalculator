85732ceb13972e61301139b6e533926bc7a76249
Michael Mhlengi Male <membee007@hotmai.com>
2013-08-16 22:10:47 +0200

    done in 27 min

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -8,118 +8,113 @@ namespace ClassLibrary1
     [TestFixture]
     public class Tests
     {
-        private Calculator _sut;
+        private StringCalculator _sut;
 
         [SetUp]
         public void Setup()
         {
-            _sut = new Calculator();
+            _sut = new StringCalculator();
         }
 
         [Test]
-        public void Emptyreturns0()
+        public void GivenemptyshouldreturnZero()
         {
-            Assert.That(_sut.Add(string.Empty), Is.EqualTo(0));
+            Assert.That(_sut.Add(string.Empty),Is.EqualTo(0));
         }
-
         [Test]
-        public void Onenumbershouldretunnumber()
+        public void GivenNumberShouldReturnTheNumber()
         {
-            Assert.That(_sut.Add("1"), Is.EqualTo(1));
+            Assert.That(_sut.Add("1"),Is.EqualTo(1));
         }
-
         [Test]
-        public void Addnumbers()
+        public void GivenNumbersShouldAddThem()
         {
-            Assert.That(_sut.Add("1,2"), Is.EqualTo(3));
+            Assert.That(_sut.Add("1,2"),Is.EqualTo(3));
         }
 
+    
         [Test]
-        public void Newlineasdelim()
+        public void AllowNewLineAsdelim()
         {
-            Assert.That(_sut.Add("1,2\n3"), Is.EqualTo(6));
+            Assert.That(_sut.Add("1\n2,3"),Is.EqualTo(6));
         }
-
+    
         [Test]
-        public void Customdelimetershouldbeallowed()
+        public void AllowcustomDelims()
         {
             Assert.That(_sut.Add("//;\n1;2"), Is.EqualTo(3));
         }
 
-        [TestCase("Negatives not allowed: -1", "-1,2")]
-        [TestCase("Negatives not allowed: -4,-5", "2,-4,3,-5")]
-        public void Negativenumbersshouldthrowexception(string msg, string input)
+        [TestCase("Negatives not allowed: -1","-1,2")]
+        [TestCase("Negatives not allowed: -4,-5","2,-4,3,-5")]
+        public void NegativeShouldthrowException(string message, string input)
         {
-            Assert.Throws(Is.TypeOf<Negativeexception>().And.Message.EqualTo(msg), () => _sut.Add(input));
+            Assert.Throws(Is.TypeOf<NegativeNumberException>().And.Message.EqualTo(message),()=>_sut.Add(input));
         }
+        
+  [Test]
+  public void NumbersgreaterthanThousandShouldBeIgnored()
+  {
+		Assert.That(_sut.Add("1001,2"),Is.EqualTo(2));
+  }
+ 
+    
+  [Test]
+  public void AnyLengthDelims()
+  {
+      Assert.That(_sut.Add("//[***]\n1***2***3"), Is.EqualTo(6));
+  }
 
-        [Test]
-        public void Numbersgreaterthan1000Shouldbeignored()
-        {
-            Assert.That(_sut.Add("1001,2"), Is.EqualTo(2));
-        }
+  [Test]
+  public void AllowMultipledelims()
+  {
+      Assert.That(_sut.Add("//[*][%]\n1*2%3"), Is.EqualTo(6));
+  }
 
-
-        [Test]
-        public void Delimitorsofanylengthshouldbeaccepted()
-        {
-            Assert.That(_sut.Add("//[***]\n1***2***3"), Is.EqualTo(6));
-        }
-
-        [Test]
-        public void Multipledelimitorsofanylengthshouldbeaccepted()
-        {
-            Assert.That(_sut.Add("//[*][%]\n1*2%3"), Is.EqualTo(6));
-        }
-
-        [Test]
-        public void AnylenMultipledelimitorsofanylengthshouldbeaccepted()
-        {
-            Assert.That(_sut.Add("//[**][%%%%]\n1**2%%%%3"), Is.EqualTo(6));
-        }
-
+  [Test]
+  public void AllowMultipledelimsanylength()
+  {
+      Assert.That(_sut.Add("//[**][%%%]\n1**2%%%3"), Is.EqualTo(6));
+  }
     }
 
-    public class Negativeexception : Exception
+    public class NegativeNumberException:Exception
     {
-        public Negativeexception(string error)
-            : base(error)
-        {
-
-        }
+        public NegativeNumberException(string error)
+            :base(error){
+        
+            }
     }
 
-    internal class Calculator
+    public class StringCalculator
     {
         public int Add(string val)
-        {
-            if (string.IsNullOrEmpty(val))
-                return 0;
-            var delims = new List<string> {",", "\n"};
+        {if(string.IsNullOrEmpty(val))
+            return 0;
+            var delimis = new List<string> {",", "\n"};
             if (val.StartsWith("//["))
             {
-                var newdels = val.Substring(3, val.IndexOf("]\n", StringComparison.Ordinal) - 3)
-                                 .Split(new[] {"]["}, StringSplitOptions.RemoveEmptyEntries);
-                delims.AddRange(newdels);
-                val = val.Substring(val.IndexOf("]\n", StringComparison.Ordinal) + 2);
+                var dels = val.Substring(3, val.IndexOf("]\n") - 3)
+                              .Split(new[] {"]["}, StringSplitOptions.RemoveEmptyEntries);
+                delimis.AddRange(dels);
+                val = val.Substring(val.IndexOf('\n') + 1);
             }
             else if (val.StartsWith("//"))
             {
-                delims.Add(val.Substring(2, 1));
-                val = val.Substring(val.IndexOf("\n", StringComparison.Ordinal) + 1);
+                delimis.Add(val[2].ToString());
+                val = val.Substring(val.IndexOf('\n') + 1);
             }
-            var nums = val.Split(delims.ToArray(), StringSplitOptions.None);
-            Checkfornegs(nums);
-            return nums.Select(int.Parse).Where(x => x <= 1000).Sum();
+            var numbers = val.Split(delimis.ToArray(),StringSplitOptions.None);
+            checkForNegatives(numbers);
+            return numbers.Select(int.Parse).Where(x=>x<=1000).Sum();
         }
 
-        private static void Checkfornegs(IEnumerable<string> nums)
+        private void checkForNegatives(IEnumerable<string> numbers)
         {
-            var negs = nums.Select(int.Parse).Where(x => x < 0);
-            if (!negs.Any()) return;
-            const string msg = "Negatives not allowed: {0}";
-            throw new Negativeexception(string.Format(msg, string.Join(",", negs)));
-
+            var negs = numbers.Select(int.Parse).Where(x => x < 0);
+            if(!negs.Any())return;
+            const string error = "Negatives not allowed: {0}";
+            throw new NegativeNumberException(string.Format(error,string.Join(",",negs)));
         }
     }
 }
